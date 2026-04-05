3245bd5c3ff0f11833ed661c5e7a1f8026d27fe8
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-05 17:35:01 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from melate_sistema_total import generar_numeros
 
-st.title("Melate AI")
+st.title("🎯 Melate AI")
 
 if st.button("Generar combinación"):
     resultado = generar_numeros()
-    st.write(resultado)
+    st.success(f"Tu combinación: {resultado}")
